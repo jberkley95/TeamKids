56ffcbad027444664788ed2bf708ff53e6bbaf38
JohnBerkley <jberkley95@gmail.com>
2016-10-23 20:07:05 -0700

    Beta Version, all items fully functioning as intended for the time being.

--- app/src/main/java/org/teamkids/teamkids/MainActivity.java
+++ app/src/main/java/org/teamkids/teamkids/MainActivity.java
@@ -6,10 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button missionButton, historyButton, whatWeDoButton, whyItMattersButton, eventsButton, mediaButton, donateButton;
+    ImageView facebookButton, twitterButton, instagramButton, vimeoButton, pinterestButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
         eventsButton = (Button) findViewById(R.id.eventsButton);
         mediaButton = (Button) findViewById(R.id.mediaButton);
         donateButton = (Button) findViewById(R.id.donateButton);
+
+        facebookButton = (ImageView) findViewById(R.id.facebookButton);
+        twitterButton = (ImageView) findViewById(R.id.twitterButton);
+        instagramButton = (ImageView) findViewById(R.id.instagramButton);
+        vimeoButton = (ImageView) findViewById(R.id.vimeoButton);
+        pinterestButton = (ImageView) findViewById(R.id.pinterestButton);
 
         missionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,6 +84,51 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Uri uri = Uri.parse("https://www.teamkids.org/donate-now");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        facebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.facebook.com/teamkids");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        twitterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://twitter.com/teamkids");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        instagramButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.instagram.com/teamkidsorg/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        vimeoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://vimeo.com/user22988862");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        pinterestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.pinterest.com/teamkids1/boards/");
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
